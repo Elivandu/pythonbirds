6a1212ed01d6b67d8b1129d8557127be0f5b0ab6
elivandus <vandussheets@gmail.com>
2023-03-26 18:15:46 -0300

    Criados atributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa():
+    def __init__(self, nome = None, idade = 0):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Ola Pessoa {id(self)}'
 
@@ -7,3 +10,5 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Elivando'
+    print(p.nome)
