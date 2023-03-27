512a8ec45df747c7d0926f3a1769954e10148cc9
elivandus <vandussheets@gmail.com>
2023-03-26 22:14:07 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,20 @@
 class Pessoa():
-    def __init__(self, nome = None, idade = 0):
+    def __init__(self, *filhos, nome = None, idade = 35):
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola Pessoa {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Elivando'
-    print(p.nome)
+    vando = Pessoa(nome='Vando')
+    elias = Pessoa(vando, nome='Elias')
+    print(Pessoa.cumprimentar(elias))
+    print(id(elias))
+    print(elias.cumprimentar())
+    print(elias.nome)
+    print(elias.idade)
+    for filho in elias.filhos:
+        print(filho.nome)
+
